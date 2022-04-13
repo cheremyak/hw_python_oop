bdc920e013d38ac436771ec9927bc0b53deb5efb
Леонид Винокуров <leoneed@me.com>
2022-04-13 19:26:51 +0500

    программа выполняется

--- homework.py
+++ homework.py
@@ -1,6 +1,26 @@
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    pass
+
+    def __init__(self,
+                 training_type: str,
+                 duration: float,
+                 distance: float,
+                 speed: float,
+                 calories: int) -> None:
+        self.training_type = training_type
+        self.duration = duration
+        self.distance = distance
+        self.speed = speed
+        self.calories = calories
+
+    def get_message(self) -> str:
+        """Вывод данных о тренировке."""
+        return (f"Тип тренировки: {self.training_type}; "
+                f"Длительность: {self.duration:.3f} ч.; "
+                f"Дистанция: {self.distance:.3f} км; "
+                f"Ср. скорость: {self.speed:.3f} км/ч; "
+                f"Потрачено ккал: {self.calories:.3f}."
+                )
 
 
 class Training:
@@ -33,7 +53,12 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        pass
+        info: InfoMessage = InfoMessage(self.__class__.__name__,
+                                        self.duration,
+                                        self.get_distance(),
+                                        self.get_mean_speed(),
+                                        self.get_spent_calories())
+        return info
 
 
 class Running(Training):
@@ -106,16 +131,17 @@ class Swimming(Training):
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
 
-    workout = {'SWM': Swimming,
-               'RUN': Running,
-               'WLK': SportsWalking}
+    workout_data = {'SWM': Swimming,
+                    'RUN': Running,
+                    'WLK': SportsWalking}
 
-    return workout[workout_type](*data)
+    return workout_data[workout_type](*data)
 
 
 def main(training: Training) -> None:
     """Главная функция."""
-    pass
+    info: InfoMessage = training.show_training_info()
+    print(info.get_message())
 
 
 if __name__ == '__main__':
